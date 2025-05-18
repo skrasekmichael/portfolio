3dd6fb8aacdea6daf1e4d220d116c4ff6976b03a
skrasek.michael <skrasek.michal@email.cz>
2025-05-18 11:18:09 +0200

    add time spans + add medito ref

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
-//render correct age in every '.age' class
-const elements = document.getElementsByClassName("age");
-const age = (new Date().getFullYear() - 2000);
-for (let i = 0; i < elements.length; i++) {
-	elements[i].innerHTML = age;
+
+loadAge();
+loadTimeSpans();
+
+function loadAge() {
+	//render correct age in every '.age' class
+	const ageElements = document.getElementsByClassName("age");
+	const age = (new Date().getFullYear() - 2000);
+	for (let i = 0; i < ageElements.length; i++) {
+		ageElements[i].innerHTML = age;
+	}
+}
+
+function loadTimeSpans() {
+	const timeSpanElements = document.querySelectorAll("[from]");
+	for (let i = 0; i < timeSpanElements.length; i++) {
+		const from = new Date(Date.parse(timeSpanElements[i].getAttribute("from")));
+		const toElement = timeSpanElements[i].getAttribute("to");
+
+		let to;
+		if (toElement == undefined) {
+			to = new Date();
+		} else {
+			to = new Date(Date.parse(toElement));
+		}
+
+		timeSpanElements[i].innerHTML = formatTimeSpan(from, to);
+	}
+}
+
+function formatTimeSpan(from, to) {
+	let years = to.getFullYear() - from.getFullYear();
+	let months = to.getMonth() - from.getMonth();
+
+	if (months < 0) {
+		years--;
+		months += 12;
+	}
+
+	let msg
+	if (years == 0) {
+		if (months == 1) {
+			msg = "1 month";
+		} else {
+			msg = `${months} months`;
+		}
+	} else {
+		msg = `${(years + months / 12).toFixed(1)} years`;
+	}
+
+	return msg;
 }
 
 //menu toggle button
